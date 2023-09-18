2120c3f0d127b33cc2d8ead85e57121a2b02df32
Francisco Alloatti <francisco.alloatti@gosysglobal.com>
2023-09-18 14:58:19 -0300

    ..

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth')->group(function () {
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/tags/index');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect('/tags/index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 });
 
